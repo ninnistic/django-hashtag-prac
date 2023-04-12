d9f0cceda4a7a1523f8466ab223b424e08ec2d55
ninnistic <parkninni@icloud.com>
2023-04-12 17:30:25 +0900

    Implement hashtag function

---
+++ movies/templatetags/make_link.py
@@ -0,0 +1,17 @@
+from django import template
+
+
+register = template.Library()
+
+# custom filter 제작
+
+
+@register.filter
+def hashtag_link(word):
+    content = word.content + ' '
+    hashtags = word.hashtags.all()
+
+    for hashtag in hashtags:
+        content = content.replace(
+            hashtag.content + ' ', f'<a href="/movies/{hashtag.pk}/hashtag/">{hashtag.content}</a> ')
+    return content
