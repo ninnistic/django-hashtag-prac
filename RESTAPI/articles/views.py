1bb1e8c916e7de2eb3a482041c0324ef712c5c7a
ninnistic <bewithu14@gmail.com>
2023-04-16 16:33:44 +0900

    Implement new request types for Article(PUT, PATCH)

--- RESTAPI/articles/views.py
+++ RESTAPI/articles/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import render
+from rest_framework import status
 # Json response
 from django.http.response import JsonResponse, HttpResponse
 from django.core import serializers
@@ -16,27 +17,33 @@ def article_html(request):
     return render(request, 'articles/article.html', context)
 
 # Json Response로 돌려주기
+
+
 def article_json_1(request):
     articles = Article.objects.all()
     articles_json = []
     for article in articles:
         articles_json.append(
             {
-            'id' : article.pk,
-            'title' : article.title,
-            'content': article.content
+                'id': article.pk,
+                'title': article.title,
+                'content': article.content
             }
         )
     return JsonResponse(articles_json, safe=False)
 
-# django.core import serializers 
+# django.core import serializers
+
+
 def article_json_2(request):
     articles = Article.objects.all()
     data = serializers.serialize('json', articles)
     return HttpResponse(data, content_type='application/json')
 
 # DRF는 decorator 붙여줘야 한다. 요청 방식도 명시
-# .serializers 에서 ArticleSerializer import 해온다.  
+# .serializers 에서 ArticleSerializer import 해온다.
+
+
 @api_view(['GET'])
 def article_json_3(request):
     articles = Article.objects.all()
@@ -44,16 +51,31 @@ def article_json_3(request):
     return Response(serializer.data)
 
 # single-model 활용하기
+
+
 @api_view(['GET'])
 def article_list(request):
     articles = Article.objects.all()
     serializer = ArticleSerializer(articles, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
+
+@api_view(['GET', 'DELETE', 'PUT', 'PATCH'])
 def article_detail(request, article_pk):
     article = Article.objects.get(pk=article_pk)
-    serializer = ArticleSerializer(article)
-    return Response(serializer.data)
-
-    
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    elif request.method == 'PATCH':
+        serializer = ArticleSerializer(article, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
